f0b76c7fcd6a8ab051888a3f4352675003ec9488
TRANDUYTINH <tranduytinh12003@gmail.com>
2024-11-17 19:21:49 +0700

    Update giao diện đổi mật khẩu

--- web-chat/src/App.js
+++ web-chat/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import ForgotPassword from "./pages/ForgotPassword";
+import ChangePassword from "./pages/ChangePassword";
 
 function App() {
     return (
@@ -9,6 +10,7 @@ function App() {
             <Routes>cd
                 <Route path="/" element={<LoginPage />} />
                 <Route path="/forgot-password" element={<ForgotPassword />} />
+                <Route path="/change-password" element={<ChangePassword />} />
             </Routes>
         </Router>
     );
